84016cd25b417e0011ac7486ce476a538d27fb6c
Codingspy <praveenshinde2022mite@gmail.com>
2025-06-20 17:28:58 +0530

    fixing api endpoints

--- backend/routes/questionRoutes.js
+++ backend/routes/questionRoutes.js
@@ -5,8 +5,8 @@ const {
   createQuestion,
 } = require('../controllers/questionController');
 
-router.get('/', getQuestions);
-router.post('/questions', createQuestion);
+router.get('/api/questions', getQuestions);
+router.post('/api/questions', createQuestion);
 
 module.exports = router;
 // This code sets up the routes for handling questions in a quiz application.
