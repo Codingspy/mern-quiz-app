0fca55dc06c1d436fd1119b47ace885dbca1eadb
Codingspy <praveenshinde2022mite@gmail.com>
2025-06-20 17:14:26 +0530

    fixing apis and routes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -4,5 +4,5 @@ const API = axios.create({
   baseURL: 'https://mern-quiz-app-um60.onrender.com',
 });
 
-export const fetchQuestions = () => API.get('/questions');
-export const addQuestion = (data) => API.post('/questions', data);
+export const fetchQuestions = () => API.get('/');
+export const addQuestion = (data) => API.post('/', data);
